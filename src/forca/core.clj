88c1e7f63cc297c09c6d8b799558e58b1aa48b7d
Assisneto <netodamasceno12@gmail.com>
2018-12-30 15:14:13 -0300

    teste fibonacci

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,6 +1,12 @@
 (ns forca.core
   (:gen-class))
 
+
+(defn fib[x]
+    (if (= x 0) 0 
+    (if (= x 1) 1 
+    (+ (fib (- x 1)) (fib (- x 2))))))
+
 (def total-de-vidas 6)
 
 (defn perdeu[] (println "Voce perdeu"))
