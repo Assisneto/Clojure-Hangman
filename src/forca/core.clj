d463d81daa39e594da44d9fabe157b185dfbca85
Assisneto <netodamasceno12@gmail.com>
2018-12-30 15:12:04 -0300

    logica inicial do jogo da forca

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -3,6 +3,19 @@
 
 (def total-de-vidas 6)
 
+(defn perdeu[] (println "Voce perdeu"))
+
+(defn jogo[vidas]
+  (if (= vidas 0)
+    (perdeu)
+    (do
+      (println vidas)
+      (jogo(dec vidas))
+
+    )
+  )
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
