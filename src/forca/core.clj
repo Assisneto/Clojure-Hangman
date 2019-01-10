c4283d5cf4cad3776d521f45c26d994e431564c1
Assisneto <netodamasceno12@gmail.com>
2019-01-09 23:25:23 -0300

    fix bug tail recursion

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,45 +1,42 @@
 (ns forca.core
   (:gen-class))
-
-
+  
 (defn fib[x]
     (if (= x 0) 0 
     (if (= x 1) 1 
     (+ (fib (- x 1)) (fib (- x 2))))))
 
+
 (def total-de-vidas 6)
 
-(defn ganhou[] (println "Voce ganhou"))
-
+(defn perdeu [] (print "Você perdeu"))
+(defn ganhou [] (print "Você ganhou!"))
 
 (defn letras-faltantes [palavra acertos]
-  (remove (fn [letra] (contains? acertos (str letra))) palavra)
-)
+    (remove (fn [letra] (contains? acertos (str letra))) palavra))
 
 (defn acertou-a-palavra-toda? [palavra acertos] 
-  (empty? (letras-faltantes palavra acertos))
+    (empty? (letras-faltantes palavra acertos))
 )
 
 (defn le-letra! [] (read-line))
 
-(defn acertou? [palavra chute] (contains? {:palavra nil} :chute) )
-
-
-(defn perdeu[] (println "Voce perdeu"))
+(defn acertou? [chute palavra] (.contains palavra chute))
 
 (defn jogo [vidas palavra acertos]
-    (cond
-        (=vidas 0) (perdeu)
+    (cond 
+        (= vidas 0) (perdeu)
         (acertou-a-palavra-toda? palavra acertos) (ganhou)
-        :else   
+        :else
         (let [chute (le-letra!)]
             (if (acertou? chute palavra)
-                (do
+                (do 
                     (println "Acertou a letra!")
                     (recur vidas palavra (conj acertos chute)))
-                (do
+                (do    
                     (println "Errou a letra! Perdeu vida!")
                     (recur (dec vidas) palavra acertos))))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
