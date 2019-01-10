8b4be8b9767bf3a257e12e0e852b55b219c0438d
Assisneto <netodamasceno12@gmail.com>
2019-01-09 22:49:03 -0300

    função de calda

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -24,24 +24,22 @@
 
 (defn acertou? [palavra chute] (contains? {:palavra nil} :chute) )
 
-(defn avalia-chute [chute vidas palavra acertos]
-    (if (acertou? palavra chute)
-        (jogo vidas palavra (conj acertos chute))
-        (jogo (dec vidas) palavra acertos)
-    )
-)
 
 (defn perdeu[] (println "Voce perdeu"))
 
 (defn jogo [vidas palavra acertos]
-  (if (= vidas 0) 
-      (perdeu)
-      (if (acertou-a-palavra-toda? palavra acertos)
-          (ganhou)
-          (avalia-chute (le-letra!) vidas palavra acertos)
-      )
-  )
-)
+    (cond
+        (=vidas 0) (perdeu)
+        (acertou-a-palavra-toda? palavra acertos) (ganhou)
+        :else   
+        (let [chute (le-letra!)]
+            (if (acertou? chute palavra)
+                (do
+                    (println "Acertou a letra!")
+                    (recur vidas palavra (conj acertos chute)))
+                (do
+                    (println "Errou a letra! Perdeu vida!")
+                    (recur (dec vidas) palavra acertos))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
